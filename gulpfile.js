569153eeba98aa7ba7cf230d9102c0e4e8be93f6
Alejandro Gómez Morón <amoron@emergya.com>
2016-02-17 16:19:51 +0100

    Removing nsp zone until it's fixed. jfigueruela will report it

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,9 @@ gulp.task('static', function () {
     .pipe(eslint.failAfterError());
 });
 
-//gulp.task('nsp', function (cb) {
-//  nsp({package: path.resolve('package.json')}, cb);
-//});
+gulp.task('nsp', function (cb) {
+  nsp({package: path.resolve('package.json')}, cb);
+});
 
 gulp.task('pre-test', function () {
   return gulp.src('generators/**/*.js')
@@ -48,5 +48,5 @@ gulp.task('watch', function () {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('prepublish', ['nsp']);
+//gulp.task('prepublish', ['nsp']);
 gulp.task('default', ['static', 'test']);
