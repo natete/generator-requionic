a8db02f7de50b55bf9b36a28b70088c8da566d80
Alejandro Gómez Morón <amoron@emergya.com>
2016-02-22 08:42:28 +0100

    #3 Adding last npm plugins

--- generators/utils/index.js
+++ generators/utils/index.js
@@ -8,12 +8,13 @@ module.exports = {
   getAvailablePlugins: function () {
     var plugins = {
       cordovaplugininappbrowser: {name: 'cordova-plugin-inappbrowser', source: 'npm', spec: '1.1.1'},
-      orgapachecordovastatusbar: {name: 'org.apache.cordova.statusbar', source: 'pgb', spec: '0.1.4'},
+      orgapachecordovastatusbar: {name: 'cordova-plugin-statusbar', source: 'npm', spec: '2.1.0'},
       cordovasqlitestorage: {name: 'cordova-sqlite-storage', source: 'npm', spec: '0.7.14'},
       ionicpluginkeyboard: {name: 'ionic-plugin-keyboard', source: 'npm', spec: '1.0.8'},
-      nlxservicespluginstoast: {name: 'nl.x-services.plugins.toast', source: 'pgb', spec: '2.0.2'},
+      nlxservicespluginstoast: {name: 'cordova-plugin-x-toast', source: 'npm', spec: '2.4.0'},
       phonegappluginpush: {name: 'phonegap-plugin-push', source: 'npm', spec: '1.5.3'},
-      cordovapluginvibration: {name: 'cordova-plugin-vibration', source: 'npm', spec: '2.1.0'}
+      cordovapluginvibration: {name: 'cordova-plugin-vibration', source: 'npm', spec: '2.1.0'},
+      cordovapluginnetworkinformation: {name: ' cordova-plugin-network-information', source: 'npm', spec: '1.2.0'}
     };
     return plugins;
   }
